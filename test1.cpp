75f8a050b103e0d6d24c32e87a7e74114b34e858
ALUMNO-D104 <ALUMNO-D104@UCSP.AULAB>
2025-08-22 08:26:44 -0500

    Añadiendo cambios del proyecto

--- test1.cpp
+++ test1.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hola mundo" << std::endl;
-    return 0;    
+
+    int val1 = 10;
+    int val2 = 20;
+    int result = val1 + val2;
+
+    std::cout << "Este es el resultado de tu compleja ecuación " << std::endl;
+    std::cout << result << std::endl;
+
+    int dni_usuario;
+
+    std::cout << "Pon tu DNI: ";
+    std::cin >> dni_usuario;
+
+    std::cout << "TU DNI es: " << dni_usuario << std::endl;
+
+    return 0;
 }
+
