c740705647f398868f314864fb97d805d43f8084
ALUMNO-B302 <ALUMNO-B302@UCSP.AULAB>
2025-08-27 07:44:07 -0500

    Añadiendo cambios al repositorio

---
+++ cpp/lab1.cpp
@@ -0,0 +1,28 @@
+// Laboratorio 1
+// Este laboratorio se especializa en el uso de operadores aritmétcos que nos proporciona c++
+
+#include <iostream>
+
+using namespace std;
+
+int var1 = 20;
+int var2 = 30;
+int resultado = 0;
+
+int main() {
+    // SUMA
+    resultado = var1 + var2;
+    cout << "La suma de esas dos variables es: " << resultado << endl;
+
+    // RESTA
+    resultado = var1 - var2;
+    cout << "La resta de esas dos variables es: " << resultado << endl;
+
+    // Multiplicación
+    resultado = var1 * var2;
+    cout << "El producto de esas dos variables es: " << resultado << endl;
+
+    // Division entera
+    resultado = var1 / var2;
+    cout << "La division entera de esas dos variables es: " << resultado << endl;
+}
---
+++ cpp/lab2.cpp
@@ -0,0 +1,14 @@
+// Laboratorio 2
+// Este laboratorio se enfoca en el registro de datos
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int var1 = 1;
+
+    cout << "Ipmrimpiendo el tamaño de la variable 1: " << sizeof(var1) << endl;
+
+    return 0;
+}
