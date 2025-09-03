d71c9ff425ea6eb4a7c0f47d755cdb07dc492a50
ALUMNO-B302 <ALUMNO-B302@UCSP.AULAB>
2025-09-03 08:21:07 -0500

    Añadiendo avance del proyecto

--- cpp/lab5.cpp
+++ cpp/lab5.cpp
@@ -1,14 +1,41 @@
+// Laboratorio 5
+// Laboratorio enfocado en la experimentación, uso y análisis de bucles
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
-    int num;
+    int i = 1;
 
-    cout << "Pon un número: ";
-    cin >> num;
+    // Imprimiendo numeros del 1 al 10
+    while (i < 10) {
+        cout << i << endl;
+        i += 1;
+    }
 
-    cout << num << endl;
+    cout << "========" << endl;
 
-    return 0;
+    // Imprimiendo numeros del 9 al 1
+    while (i >= 1) {
+        cout << i << endl;
+        i -= 1;
+    }
+
+    cout << "========" << endl;
+
+    int num = 1;
+
+    // Imprimiendo multiplos de 3 menores de 100
+    while (num < 100) {
+        if ((num % 3) == 0) {
+            cout << num << " ";
+        }
+
+        num += 1;
+    }
+
+    cout << endl;
+
+    cout << "========" << endl;
 }
---
+++ cpp/lab7.cpp
@@ -0,0 +1,41 @@
+// Laboratorio 7
+// Números primos
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num = 2;
+    int rango;
+
+    cout << "Pon un numero: ";
+    cin >> rango;
+    cout << endl;
+
+    int mitad = 0;
+
+    while (num < rango) {
+        mitad = (num / 2) + 1;
+        int middle_rango = 2;
+        int primo = true;
+
+        while (middle_rango < mitad) {
+            if ((num % middle_rango) == 0) {
+                primo = false;
+                //cout << "Divisor: " << middle_rango << " Num: " << num << endl;
+                break;
+            }
+
+            middle_rango += 1;
+        }
+
+        if (primo) {
+            cout << "El " << num << " es primo" << endl;
+        }
+
+        num += 1;
+    }
+
+    return 0;
+}
